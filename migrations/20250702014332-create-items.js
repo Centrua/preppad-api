aaac012d1df0e627e52de349757fd9ee1da7bfe9
Ian Gongwer <iangongwerpersonal@gmail.com>
2025-07-04 13:55:07 -0400

    finished items migration

--- migrations/20250702014332-create-items.js
+++ migrations/20250702014332-create-items.js
@@ -3,17 +3,17 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('Item', {
-      itemid: {
+    await queryInterface.createTable('Items', {
+      itemId: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         allowNull: false
       },
-      itemname: {
+      itemName: {
         type: Sequelize.STRING(255),
         allowNull: false
       },
-      unitcost: {
+      unitCost: {
         type: Sequelize.DECIMAL(6, 2),
         allowNull: false
       },
@@ -24,29 +24,33 @@ module.exports = {
       upc: {
         type: Sequelize.BIGINT
       },
-      expirationdate: {
+      expirationDate: {
         type: Sequelize.DATE
       },
       unit: {
         type: Sequelize.STRING(50),
         allowNull: false
       },
-      quantityinstock: {
+      quantityInStock: {
         type: Sequelize.INTEGER,
         allowNull: false
       },
-      isperishable: {
+      isPerishable: {
         type: Sequelize.CHAR(1),
         allowNull: false
       },
-      lastupdated: {
-        type: Sequelize.DATE,
-        allowNull: false
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
       }
     });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('Item');
+    await queryInterface.dropTable('Items');
   }
 };
--- models/item.js
+++ models/item.js
@@ -1,52 +1,53 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('./your-sequelize-instance'); // Replace with your actual instance
+'use strict';
+const { Model } = require('sequelize');
 
-const Item = sequelize.define('Item', {
-  itemid: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    allowNull: false
-  },
-  itemname: {
-    type: DataTypes.STRING(255),
-    allowNull: false
-  },
-  unitcost: {
-    type: DataTypes.DECIMAL(6, 2),
-    allowNull: false
-  },
-  vendor: {
-    type: DataTypes.STRING(255),
-    allowNull: false
-  },
-  upc: {
-    type: DataTypes.BIGINT
-  },
-  expirationdate: {
-    type: DataTypes.DATE
-  },
-  unit: {
-    type: DataTypes.STRING(50),
-    allowNull: false
-  },
-  quantityinstock: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  isperishable: {
-    type: DataTypes.CHAR(1),
-    allowNull: false,
-    validate: {
-      isIn: [['Y', 'N']]
+module.exports = (sequelize, DataTypes) => {
+  class Item extends Model {
+    static associate(models) {
+      // define associations here if needed
     }
-  },
-  lastupdated: {
-    type: DataTypes.DATE,
-    allowNull: false
   }
-}, {
-  tableName: 'Item',
-  timestamps: false
-});
 
-module.exports = Item;
+  Item.init({
+    itemId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    itemName: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    unitCost: {
+      type: DataTypes.DECIMAL(6, 2),
+      allowNull: false
+    },
+    vendor: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    upc: {
+      type: DataTypes.BIGINT
+    },
+    expirationDate: {
+      type: DataTypes.DATE
+    },
+    unit: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    quantityInStock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    isPerishable: {
+      type: DataTypes.CHAR(1),
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Item',
+  });
+
+  return Item;
+};
